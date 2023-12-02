b628a8d16b9dfeedab217a6752052f922cd64f27
Todd Stellanova <tstellanova@users.noreply.github.com>
2023-12-02 12:03:50 -0800

    tuning countdown example

--- examples/countdown.rs
+++ examples/countdown.rs
@@ -1,5 +1,6 @@
 extern crate rv3028c7_rtc;
 
+use std::ops::Add;
 use linux_embedded_hal::I2cdev;
 use chrono::{Duration, Utc};
 use rv3028c7_rtc::{RV3028};
@@ -8,45 +9,57 @@ use rtcc::DateTimeAccess;
 use embedded_hal::blocking::i2c::{Write, Read, WriteRead};
 
 
-fn test_one_shot_duration<I2C,E>(rtc: &mut RV3028<I2C>, dur: &Duration)  -> Result<Duration, E>
+
+fn test_one_shot_duration<I2C,E>(rtc: &mut RV3028<I2C>, duration: &Duration) -> Result<Duration, E>
   where
     I2C: Write<Error = E> + Read<Error = E> + WriteRead<Error = E>,
     E: std::fmt::Debug
 {
   rtc.clear_all_int_out_bits()?;
   rtc.toggle_countdown_timer(false)?;
-  rtc.setup_countdown_timer(dur, false)?;
+  rtc.check_and_clear_countdown()?;
+  let estimated_duration = rtc.setup_countdown_timer(duration, false)?;
+  let expected_sleep =
+    if estimated_duration.le(&Duration::microseconds(976)) {
+      estimated_duration.add(Duration::microseconds(245))
+    } else if estimated_duration.le(&Duration::milliseconds(990)) {
+      estimated_duration.add(Duration::milliseconds(48))
+    }
+    else {
+      estimated_duration.add(Duration::milliseconds(16))
+    };
 
-  let expected_sleep = dur.to_std().unwrap();
+
+
   let start_time = Utc::now().naive_utc();
 
-  println!("sleep {} ", dur);
+  println!("> oneshot {} sleep {} ", duration, expected_sleep);
   rtc.toggle_countdown_timer(true)?;
-  spin_sleep::sleep(expected_sleep);
-  // std::thread::sleep(expected_sleep);
+  if estimated_duration.lt(&Duration::seconds(1)) {
+    std::thread::sleep(expected_sleep.to_std().unwrap());
+  }
+  else {
+    spin_sleep::sleep(expected_sleep.to_std().unwrap());
+  }
 
-  let mut last_remaining = 555;
   let actual = loop {
-    if let Ok(true) = rtc.check_and_clear_countdown() {
+    let remain = rtc.get_countdown_value()?;
+    if 0 == remain {
+      let triggered = rtc.check_and_clear_countdown()?;
+      if !triggered { println!("Counter zero but PERIODIC_TIMER_FLAG untriggered!!")}
       let end_time = Utc::now().naive_utc();
-      let delta =  end_time - start_time ;
+      let delta = end_time - start_time;
       break delta;
     }
     else {
-      let val =     rtc.get_countdown_value()?;
-      println!("remain: {}", val);
-      if 0 == val {
-        if 0 == last_remaining {
-          let end_time = Utc::now().naive_utc();
-          let delta = end_time - start_time;
-          break delta;
-        }
-        last_remaining = 0;
-      }
+      println!("remain: {}", remain);
+      // 15.625 ms uncertainty
+      std::thread::sleep(Duration::milliseconds(1).to_std().unwrap());
     }
   };
 
-  println!("actual {} expected {}", actual, dur);
+  let delta_total = actual - estimated_duration ;
+  println!("< actual {} expected {} requested {} delta {}", actual, estimated_duration, duration, delta_total);
   Ok(actual)
 }
 
@@ -57,51 +70,47 @@ fn test_periodic_duration<I2C,E>(rtc: &mut RV3028<I2C>, duration: &Duration) -> 
 {
   rtc.clear_all_int_out_bits()?;
   rtc.toggle_countdown_timer(false)?;
+  rtc.check_and_clear_countdown()?;
+
   let estimated_duration = rtc.setup_countdown_timer(duration, true)?;
-  let expected_sleep = estimated_duration.to_std().unwrap();
+  // we don't adjust for the first duration uncertainty, assuming it will average out
+  let expected_sleep =    estimated_duration.add(Duration::milliseconds(16)) ;
+  println!("> periodic {} sleep {} ", duration, expected_sleep);
 
   // place to store the sum of all measured countdown durations
   let mut sum_actual = Duration::zero();
   const NUM_ITERATIONS: usize = 5;
-
+  
+  // start the countdown repeating
   rtc.toggle_countdown_timer(true)?;
+
+  let mut start_time = Utc::now().naive_utc();
   for _i in 0..NUM_ITERATIONS {
-    let start_time = Utc::now().naive_utc();
-    // println!("sleep {} ", duration);
-    spin_sleep::sleep(expected_sleep);
-    // std::thread::sleep(expected_sleep);
+    if estimated_duration.lt(&Duration::seconds(1)) {
+      std::thread::sleep(expected_sleep.to_std().unwrap());
+    }
+    else {
+      spin_sleep::sleep(expected_sleep.to_std().unwrap());
+    }
 
-    let mut check_count = 0;
-    let actual =
-      loop {
-        if let Ok(true) = rtc.check_countdown_finished() {
-          rtc.check_and_clear_countdown()?;
-          let end_time = Utc::now().naive_utc();
-          let delta = end_time - start_time;
-          break delta;
-        }
-        else {
-          check_count += 1;
-          if check_count > 5 {
-            let remain = rtc.get_countdown_value()?;
-            println!("remain: {}", remain);
-            // if 0 == remain {
-                let end_time = Utc::now().naive_utc();
-                let delta = end_time - start_time;
-                break delta;
-            // }
-          }
-        }
-      };
+    let actual = loop {
+      let triggered = rtc.check_and_clear_countdown()?;
+      if triggered {
+        let end_time = Utc::now().naive_utc();
+        let delta = end_time - start_time;
+        start_time = end_time; //reset timer for next event
+        break delta;
+      }
+    };
 
-    println!("actual {} expected {}", actual, duration);
-    rtc.toggle_countdown_timer(false)?;
-
+    // println!("actual {} expected {}", actual, duration);
     sum_actual = sum_actual + actual;
   }
 
   let avg_actual = sum_actual / NUM_ITERATIONS as i32;
-  println!("avg_actual {} expected {} requested {}", avg_actual, estimated_duration, duration);
+  let delta_total = avg_actual - estimated_duration ;
+  println!("< avg_actual {} expected {} requested {} delta: {}",
+           avg_actual, estimated_duration, duration, delta_total);
 
   Ok(())
 }
@@ -120,21 +129,22 @@ fn main() {
   let dt_rtc = rtc.datetime().unwrap();
   println!("sys {}\r\nrtc {}\r\n", dt_sys, dt_rtc);
 
-  test_periodic_duration(&mut rtc, &Duration::milliseconds(45)).unwrap();
 
-  // test_one_shot_duration(&mut rtc, &Duration::microseconds(244)).unwrap();
-  // test_one_shot_duration(&mut rtc, &Duration::microseconds(488)).unwrap();
-  // test_one_shot_duration(&mut rtc, &Duration::microseconds(976)).unwrap();
-  // test_one_shot_duration(&mut rtc, &Duration::microseconds(976)).unwrap();
-  // test_one_shot_duration(&mut rtc, &Duration::microseconds(999180)).unwrap();
-  //
-  // test_one_shot_duration(&mut rtc, &Duration::milliseconds(15)).unwrap();
-  // test_one_shot_duration(&mut rtc, &Duration::milliseconds(30)).unwrap();
-  // test_one_shot_duration(&mut rtc, &Duration::milliseconds(45)).unwrap();
-  //
-  // test_one_shot_duration(&mut rtc, &Duration::seconds(30)).unwrap();
-  // test_one_shot_duration(&mut rtc, &Duration::minutes(2)).unwrap();
-  // test_one_shot_duration(&mut rtc, &Duration::seconds(300)).unwrap();
+  // Note that all RTC durations come with a minimum uncertainty in the
+  // first period duration.
+  // This is between 244 microseconds and 15 milliseconds
+
+  println!("\r\n==== ONE SHOTS ====");
+  test_one_shot_duration(&mut rtc, &Duration::microseconds(488)).unwrap();
+  test_one_shot_duration(&mut rtc, &Duration::milliseconds(15 * (1000/15))).unwrap();
+  test_one_shot_duration(&mut rtc, &Duration::seconds(3)).unwrap();
+  // test_one_shot_duration(&mut rtc, &Duration::minutes(1)).unwrap();
+
+  println!("\r\n==== PERIODICS ====");
+  test_periodic_duration(&mut rtc, &Duration::microseconds(488)).unwrap();
+  test_periodic_duration(&mut rtc, &Duration::milliseconds(15 * (1000/15))).unwrap();
+  test_periodic_duration(&mut rtc, &Duration::seconds(3)).unwrap();
+  // test_periodic_duration(&mut rtc, &Duration::minutes(1)).unwrap();
 
 
 
